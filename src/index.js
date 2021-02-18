d45ea11be37b69fc68e83e3d592cc22e74944f3b
AkiraHuynh <akiramichiokiyoshi@gmail.com>
2021-02-18 13:51:00 +0900

    202102181323

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan=require('morgan');
+const handlebars=require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('handlebars',handlebars());
+app.set('view engine','handlebars');
+app.get('/trang-chu', (req, res) => {
+    
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
